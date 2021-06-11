1acf718ca7adc5e4ea93abe3d55b710faf1b552b
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-10 21:16:12 -0500

    Moved Home to containers

--- frontend/src/containers/App.js
+++ frontend/src/containers/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from '../components/Header'
-import Home from '../components/Home'
+import Home from '../containers/Home'
 import About from '../components/About'
 import Races from '../containers/Races'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
