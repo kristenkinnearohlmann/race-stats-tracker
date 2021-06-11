dea1618d48d20200f3d0e3763fdcbc9efcc209cc
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-10 21:29:45 -0500

    Created backend User resource

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create(email: "focusrunning.kko@gmail.com", password: "abc123", date_of_birth: "1976-01-06", bio: "A runner from Bloomington, MN")
