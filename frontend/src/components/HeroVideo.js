adc24ef0c8064222c79ecc871b905f64b010c3ef
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-10 21:23:37 -0500

    Added URL to poster image for hero video

--- frontend/src/components/HeroVideo.js
+++ frontend/src/components/HeroVideo.js
@@ -2,11 +2,12 @@ import React from 'react'
 import './HeroVideo.css'
 
 const HeroVideo = () => {
-    const url = 'https://res.cloudinary.com/du4oldiab/video/upload/v1623376539/race-stats-tracker/running_09_videvo_wkncxu.mp4'
+    const poster = "https://res.cloudinary.com/du4oldiab/image/upload/v1623378148/race-stats-tracker/running_09_videvo_poster_a6w9sg.jpg"
+    const video = 'https://res.cloudinary.com/du4oldiab/video/upload/v1623376539/race-stats-tracker/running_09_videvo_wkncxu.mp4'
     return (
         <div>
             <video id="hero-video" autoPlay muted loop>
-                <source src={url} type='video/mp4' />
+                <source src={video} poster={poster} type='video/mp4' />
                 Your browser does not support video.
             </video>
         </div>
