9fbd75c9596e1ee9c500e0ed749a76f5481b5823
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-11 20:11:21 -0500

    Connected Races container to Redux store

--- frontend/src/containers/Races.js
+++ frontend/src/containers/Races.js
@@ -1,7 +1,14 @@
 import { React, Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchRaceDistances } from '../actions/racesActions'
 import RaceDistanceDropdown from '../components/RaceDistanceDropdown'
 
 class Races extends Component {
+
+    componentDidMount() {
+        this.props.fetchRaceDistances()
+    }
+    
     render() {
         return (
             <div>
@@ -14,4 +21,17 @@ class Races extends Component {
     }
 }
 
-export default Races
+const mapStateToProps = state => {
+    return {
+        race_distances: state.race_distances,
+        loading: state.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchRaceDistances: () => dispatch(fetchRaceDistances())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Races)
