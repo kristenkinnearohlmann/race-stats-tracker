70f55664ef0dabdaf52a0f7b686236027b09c4ff
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-11 21:16:08 -0500

    Starting work on race distance select dropdown

--- frontend/src/components/RaceDistanceDropdown.js
+++ frontend/src/components/RaceDistanceDropdown.js
@@ -2,10 +2,22 @@ import React from 'react'
 
 const RaceDistanceDropdown = ({ raceDistances }) => {
     console.log(raceDistances)
+    console.log(raceDistances.forEach(distance => {
+        console.log(distance.id)
+        console.log(distance.distance_type)
+    }))
     return (
-        <>
-            <p>Race Distance Dropdown here</p>
-        </>
+        <form>
+            <select name="raceDistances" id="raceDistances">
+                <option value="">Select distance</option>
+                <option value="0">All</option>
+                {raceDistances.map(distance => {
+                    return (
+                        <option value={distance.id}>{distance.distance_type}</option>
+                    )
+                })}
+            </select>
+        </form>
     )
 }
 
