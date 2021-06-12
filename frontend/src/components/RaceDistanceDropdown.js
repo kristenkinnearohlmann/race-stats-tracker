c2324fbefc70c8ad71709d964e5db3cf2f76ea62
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-11 21:26:25 -0500

    Determining how to access values in race distance dropdown

--- frontend/src/components/RaceDistanceDropdown.js
+++ frontend/src/components/RaceDistanceDropdown.js
@@ -2,15 +2,16 @@ import React from 'react'
 import './RaceDistanceDropdown.css'
 
 const RaceDistanceDropdown = ({ raceDistances }) => {
+    console.log(raceDistances)
     return (
         <form>
             <label>Select race distance: </label>
-            <select name="raceDistances" id="raceDistances">
+            <select name="raceDistances" id="raceDistances" onChange={(event) => alert(event.target.value)}>
                 <option value=""></option>
                 <option value="0">All</option>
                 {raceDistances.map(distance => {
                     return (
-                        <option value={distance.id}>{distance.distance_type}</option>
+                        <option key={distance.id} value={distance.id}>{distance.distance_type}</option>
                     )
                 })}
             </select>
