4883f2c3521e86a9002a74fff6a8c50623bb4b53
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-12 09:37:21 -0500

    Working on form and styles for Race New

--- frontend/src/containers/RaceNew.js
+++ frontend/src/containers/RaceNew.js
@@ -1,19 +1,57 @@
-import React from 'react'
-import { Component } from 'react'
+import { React, Component } from 'react'
+import { connect } from 'react-redux'
+import { fetchRaceDistances } from '../actions/racesActions'
 import './RaceNew.css'
 
 class RaceNew extends Component {
+
+    state = {
+        name: '',
+        raceDistances: 0
+    }
+
+    componentDidMount() {
+        this.props.fetchRaceDistances()
+    }
 
     render() {
         return (
             <div className="race-new-wrapper">
                 <p>Add a race to your race list</p>
                 <form>
-
+                    <p>
+                        <label>Race name: </label><br/>
+                        <input type="text" name="name" value={this.state.name} />
+                    </p>
+                    <p>
+                        <label>Select race distance: </label><br/>
+                        <select name="raceDistances" id="raceDistances">
+                            <option value=""></option>
+                            <option value="0">All</option>
+                            {this.props.raceDistances.map(distance => {
+                                return (
+                                    <option key={distance.id} value={distance.id}>{distance.distance_type}</option>
+                                )
+                            })}
+                        </select>
+                    </p>
                 </form>
             </div>
         )
     }
 }
 
-export default RaceNew
+const mapStateToProps = state => {
+    return {
+        raceDistances: state.race_distances,
+        loading: state.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        fetchRaceDistances: () => dispatch(fetchRaceDistances()),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(RaceNew)
