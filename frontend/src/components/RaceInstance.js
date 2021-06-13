19b9b8c164c937e9e3ff3146c3af75b88c2cea8a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-13 17:18:53 -0500

    Worked on date display

--- frontend/src/components/RaceInstance.js
+++ frontend/src/components/RaceInstance.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 const RaceInstance = ({ name, url, race }) => {
     console.log(race)
+    console.log(race.race_date)
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    console.log(typeof Date.parse(race.race_date))
+    const race_date_date = new Date(race.race_date)
+    console.log(race_date_date.toLocaleDateString('en-US',options))
+    // console.log(event.toLocaleDateString('en-US', options));
     return (
         <>
             <p>
@@ -9,7 +15,8 @@ const RaceInstance = ({ name, url, race }) => {
                 <a href={url} target="blank">{url}</a>
             </p>
             <p>
-                {race.race_date}<br/>
+                {race_date_date.toLocaleDateString('en-US',options)}
+                <br/>
                 {race.elapsed_time}
             </p>
         </>
