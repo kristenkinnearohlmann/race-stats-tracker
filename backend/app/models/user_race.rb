89ce555a1699700a50a24bde1d60cb75e9eb9e2f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-13 09:46:27 -0500

    Added pry for debugging, added calculate_race_age for UserRace

--- backend/app/models/user_race.rb
+++ backend/app/models/user_race.rb
@@ -1,4 +1,15 @@
 class UserRace < ApplicationRecord
   belongs_to :user
   belongs_to :race
+
+  before_save :calculate_race_age
+
+  def calculate_race_age
+    dob = self.user.date_of_birth
+    event_dt = self.race_date
+
+    race_age_months = (event_dt.year * 12 + event_dt.month) - (dob.year * 12 + dob.month)
+    self.age = race_age_months / 12
+  end
+
 end
