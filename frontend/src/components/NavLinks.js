b56fa4649c31074ba61343a4f03db891429827ab
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-12 20:04:33 -0500

    Renamed Add Your Race link to better reflect the action taking place

--- frontend/src/components/NavLinks.js
+++ frontend/src/components/NavLinks.js
@@ -5,7 +5,7 @@ import './NavLinks.css'
 const NavLinks = () => {
     return (
         <div className="nav">
-            <NavLink to="/races/new" exact className="nav-link-base">Add a Race</NavLink>
+            <NavLink to="/races/new" exact className="nav-link-base">Add Your Race</NavLink>
             <NavLink to="/races" exact className="nav-link-base">Races</NavLink>
             <NavLink to="/about" exact className="nav-link-base">About</NavLink>
             <NavLink to="/" exact className="nav-link-base">Home</NavLink>
