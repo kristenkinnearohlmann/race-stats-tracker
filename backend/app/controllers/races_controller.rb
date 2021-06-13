38cf1870379df4195f7a6f71c64114278ab1c31a
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-12 19:43:59 -0500

    Completed basic implementation of creating a user race

--- backend/app/controllers/races_controller.rb
+++ backend/app/controllers/races_controller.rb
@@ -1,28 +1,23 @@
 class RacesController < ApplicationController
 
     def create
-        byebug
-        # params
-        # TODO: Confirm current user as user
         user = User.find(params[:current_user_id])
 
         if user
-            byebug
             race = Race.find_or_create_by(name: params[:race][:name]) do |r|
                 r.race_distance_id = params[:race][:race_distance_id]
             end
+            params[:user_race][:race_id] = race.id
+            byebug
+            user.user_races.create(race_params(params[:user_race]))
             byebug
         end
 
-        # TODO: race = Race.find_or_create_by(name: params[:race][:name]) + race_distance_id
-        # TODO: user.user_races.create(race.id,race_params([:user_race]))
-        # race_id
-        # {"current_user_id"=>1, "race"=>{"name"=>"Charleston Marathon", "race_distance_id"=>"6"}, "user_race"=>{"race_date"=>"2020-01-11", "elapsed_time"=>"", "gun_time"=>"", "results_url"=>"", "bib_nbr"=>"", "division_name"=>"", "overall_place"=>"", "overall_finishers"=>"", "gender_place"=>"", "gender_finishers"=>"", "division_place"=>"", "division_finishers"=>"", "race_notes"=>""}
     end
 
     private
 
     def race_params(*args)
-        params.require(:user_race).permit(*args)
+        params.require(:user_race).permit(:race_date, :elapsed_time, :gun_time, :results_url, :bib_nbr, :division_name, :overall_place, :overall_finishers, :gender_place, :gender_finishers, :division_place, :division_finishers, :race_notes, :race_id)
     end
 end
