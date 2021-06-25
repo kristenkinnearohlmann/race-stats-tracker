b9fff04902c99683c12a0f2e5c62db11c82347ea
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-24 23:00:35 -0500

    Started working on tests

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,13 +2,15 @@ import React from 'react'
 import logo from '../assets/logo.svg'
 import NavLinks from './NavLinks'
 import './Header.css'
-import { Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <div className="app-header-container">
-            <div><Link to="/"><img src={logo} className="app-logo" alt="logo" /><span>Race Stats Tracker</span></Link></div>
-            <div><NavLinks /></div>
+            <Router>
+                <div><Link to="/"><img src={logo} className="app-logo" alt="logo" /><span>Race Stats Tracker</span></Link></div>
+                <div><NavLinks /></div>
+            </Router>
         </div>
     )
 }
---
+++ frontend/src/tests/Header.test.js
@@ -0,0 +1,10 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import Header from '../components/Header';
+
+test('Renders Race Stats Tracker name', () => {
+    render(<Header />);
+    const textContent = screen.getAllByText(/Race Stats Tracker/i);
+    // expect(textContent).toBeInTheDocument();
+    expect(textContent).toEqual
+});
