8e04bed27bc55b07f19cbb9a890a5963974caa1f
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-25 21:37:00 -0500

    Removed unused App test, cleaned up Header test

--- frontend/src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../containers/App';
-
-test('renders learn react link', () => {
-  // render(<App />);
-  // // const linkElement = screen.getByText(/learn react/i);
-  // const linkElement = screen.getAllByText(/Race Stats Tracker/i);
-  // expect(linkElement).toBeInTheDocument();
-});
