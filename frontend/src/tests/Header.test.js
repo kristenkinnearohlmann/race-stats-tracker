46c92fdfa6fd0154523f00395152701b2956a016
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-26 21:42:14 -0500

    Passing test but still not sure how to properly create a Jest test

--- frontend/src/tests/Header.test.js
+++ frontend/src/tests/Header.test.js
@@ -1,9 +1,27 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from '../components/Header';
 
 test('Renders Race Stats Tracker name', () => {
     // render(<Header />);
     // const textContent = screen.getAllByText(/Race Stats Tracker/i);
     // expect(textContent).toEqual
+    // header = (
+    //     <Router>
+    //         <Header />
+    //     </Router>
+    // )
+    // render(header);
+    const div = document.createElement("div");
+    ReactDOM.render(
+        <Router>
+            <Header />
+        </Router>,
+        div
+    );
+
+    ReactDOM.unmountComponentAtNode(div);
 });
