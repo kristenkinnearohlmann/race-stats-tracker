51f48e1232660a17c292b31adef7fda9f035e361
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2021-06-29 20:53:55 -0500

    Added sort by race date to Race component for display

--- frontend/src/components/Race.js
+++ frontend/src/components/Race.js
@@ -2,7 +2,19 @@ import React from 'react'
 import RaceInstance from './RaceInstance'
 
 const Race = ({ races }) => {
-    const generateRaceInstances = races.map(race => <div key={race.raceLocatorId}><RaceInstance key={race.raceLocatorId} race={race} /></div>)
+    const sortedRaces = races.sort((a,b) => {
+        const dateA = a.race_date
+        const dateB = b.race_date
+        if (dateA < dateB) {
+            return 1
+        }
+        if (dateA > dateB) {
+            return -1
+        }
+        return 0
+    })
+
+    const generateRaceInstances = sortedRaces.map(race => <div key={race.raceLocatorId}><RaceInstance key={race.raceLocatorId} race={race} /></div>)
 
 
     return (
